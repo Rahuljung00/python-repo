c9cd0581ddd016934436a3208dff0cf69e1b4543
Rahul Karki <karkirahul1409@gmail.com>
2024-10-01 12:20:04 +0545

    new filw

---
+++ oddeven.py
@@ -0,0 +1,12 @@
+# odd and even number b
+
+def oddeven():
+    num1=int(input("enter a first number"))
+    if num1 % 2 == 0:
+            print(num1, "is an even number")
+    else:
+            print(num1, "is an odd number")
+
+oddeven()
+
+
